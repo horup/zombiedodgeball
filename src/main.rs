25d685c2b24ff44ea80f5e748565b25350ff8d1e
Horup <sah@sah.dk>
2020-10-26 08:31:51 +0100

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use ggez::{graphics, Context, ContextBuilder, GameResult};
+use ggez::{Context, ContextBuilder, graphics::DrawParam, GameResult, graphics::{self, GlBackendSpec, ImageGeneric}};
 use ggez::event::{self, EventHandler};
 mod state;
 use state::State;
-
+use ggez::context::*;
 pub fn main() {
     let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
         .build()
@@ -14,8 +14,12 @@ pub fn main() {
     }
 }
 
+struct Images {
+    pub spritesheet:ImageGeneric<GlBackendSpec>
+}
 struct ZombieDodgeBall {
-    pub current:State
+    pub current:State,
+    pub images:Images
 }
 
 impl ZombieDodgeBall {
@@ -23,23 +27,28 @@ impl ZombieDodgeBall {
         let bytes = include_bytes!("./resources/spritesheet.png");
         let img = image::load_from_memory(bytes).unwrap();
         
-        let img = img.to_rgba();
-        let img = graphics::Image::from_rgba8(ctx, img.width() as u16, img.height() as u16, &img).unwrap();
+        let spritesheet = img.to_rgba();
+        let spritesheet = graphics::Image::from_rgba8(ctx, spritesheet.width() as u16, spritesheet.height() as u16, &spritesheet).unwrap();
 
 
         ZombieDodgeBall {
-		    current: State::new()
+            current: State::new(),
+            images: Images {
+                spritesheet:spritesheet
+            }
 		}
     }
 }
 
 impl EventHandler for ZombieDodgeBall {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let keyboard = &ctx.keyboard_context;
 		Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-		graphics::clear(ctx, graphics::BLACK);
+        graphics::clear(ctx, graphics::BLACK);
+        graphics::draw(ctx,   &self.images.spritesheet, DrawParam::new())?;
 		graphics::present(ctx)
     }
 }
