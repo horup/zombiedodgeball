637add090c015cdf2a4e1ad398c2000d5ebd8b2c
Horup <sah@sah.dk>
2020-10-27 14:08:46 +0100

    added interpolation

--- src/render.rs
+++ src/render.rs
@@ -1,3 +1,4 @@
+use cgmath::Vector2;
 use ggez::{Context, GameResult, graphics::DrawParam, graphics::{self, Rect}};
 
 use crate::ZombieDodgeBall;
@@ -7,6 +8,7 @@ impl ZombieDodgeBall
     pub fn render(&mut self, ctx: &mut Context, alpha:f32) -> GameResult<()>
     {
         let current = &self.current;
+        let previous = &self.previous;
         let size = graphics::size(ctx);
         let zoom = 32.0;
         let sprite_size = 16.0;
@@ -18,14 +20,19 @@ impl ZombieDodgeBall
         })?;
         graphics::clear(ctx, graphics::BLACK);
 
-        for (id, e) in current.entities.iter()
+        for (id, curr) in current.entities.iter()
         {
-            let mut params = DrawParam::new();
-            params.dest.x = e.pos.x;
-            params.dest.y = e.pos.y;
-            params.scale.x = 1.0 / sprite_size;
-            params.scale.y = 1.0 / sprite_size;
-            graphics::draw(ctx,   &self.images.spritesheet, params)?;
+            if let Some((_, prev)) = previous.entities.get_entity(id)
+            {
+                let v:Vector2<f32> = (curr.pos - prev.pos) * alpha;
+                
+                let mut params = DrawParam::new();
+                params.dest.x = prev.pos.x + v.x;
+                params.dest.y = prev.pos.y + v.y;
+                params.scale.x = 1.0 / sprite_size;
+                params.scale.y = 1.0 / sprite_size;
+                graphics::draw(ctx,   &self.images.spritesheet, params)?;
+            }
         }
 
 		graphics::present(ctx)
