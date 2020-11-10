69f89905ff1d730025204dad916a5d7a648409bd
horup <sah@sah.dk>
2020-11-10 06:20:02 +0100

    added working collision handling

--- src/functions/physics.rs
+++ src/functions/physics.rs
@@ -1,11 +1,12 @@
-use cgmath::{Point2, Vector2};
+use cgmath::{Point2, Vector2, prelude::*};
 use collision::{Aabb2, prelude::*};
 use gamestate::{Collection, ID};
 
 use crate::data::{Entity, State};
 use crate::data::Event;
-use super::{util::find_player_entity_mut};
+use super::{util::{find_player_entity, find_player_entity_mut}};
 
+#[derive(Copy, Clone)]
 pub struct Body {
     pub id:ID,
     pub pos:Point2<f32>,
@@ -36,9 +37,40 @@ pub fn collision_check(entity:&Entity, other_entity:&Entity) -> bool
     e1.aabb2().intersects(&e2.aabb2())
 }
 
-pub fn sub_step(entity:&Entity, all_entities:&Collection<Entity>)
+pub fn sub_step(body:&mut Body, all_entities:&Collection<Entity>)
 {
+    let max = 0.1;
+    let distance = body.vel.magnitude();
+    let mut remaining = distance;
+    while remaining > 0.0 {
+        let step = if distance < max { distance} else {max};
+        let dir = body.vel.normalize();
+        let vs = [Vector2::new(0.0, dir.y * step), Vector2::new(dir.x * step, 0.0)];
+        for v in vs.iter()
+        {
+            let body_before = *body;
+            body.pos += *v;
+            let mut collision = false;
+            for e in all_entities.iter() {
+                if e.id == body.id {
+                    continue;
+                }
+                
+                let other_body = Body::from(e);
+                if body.aabb2().intersects(&other_body.aabb2())
+                {
+                    collision = true;
+                    break;
+                }
+            }
     
+            if collision {
+                *body = body_before;
+            }
+        }
+
+        remaining -= step;
+    }
 }
 
 pub fn step(state:&mut State, is_server:bool, events:&[Event])
@@ -48,8 +80,18 @@ pub fn step(state:&mut State, is_server:bool, events:&[Event])
     // update players
     for e in events {
         if let Event::PlayerMove(player_id, v) = e {
-            if let Some(player_entity) = find_player_entity_mut(&mut state.entities, &player_id) {
-              
+            if let Some(player_entity) = find_player_entity(&state.entities, &player_id) {
+                let mut body = Body::from(&*player_entity);
+                body.vel = *v; 
+                sub_step(&mut body, &state.entities);
+
+                if let Some(player_entity) = state.entities.get_entity_mut(player_entity.id) {
+                    player_entity.pos = body.pos;
+                    player_entity.vel = body.vel;
+                }
+                //player_entity.pos = body.pos;
+                //player_entity.vel = body.vel;
+
               //  sub_step(&player_entity, &all_entities);
 
                 /*  player_entity.1.pos += *v;
